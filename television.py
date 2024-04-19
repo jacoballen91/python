8e8e48f160f4ddb68ac03a185a6e1b414da28d53
Jacob Allen <jacoballen@unomaha.edu>
2024-04-18 21:32:57 -0500

    telivision.py update & test_television.py created
    Added type hinting and docstrings to television.py
    created test_television.py to test functionality

--- television.py
+++ television.py
@@ -4,52 +4,74 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.__status = False
         self.__muted = False
         self.__volume = 0
         self.__channel = 0
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Method to toggle television power
+        """
         if not self.__status:
             self.__status = True
         else:
             self.__status = False
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Method to toggle television mute status
+        """
         if self.__status:
             if not self.__muted:
                 self.__muted = True
             else:
                 self.__muted = False
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Method to increase television channel
+        """
         if self.__status:
             if self.__channel < self.MAX_CHANNEL:
                 self.__channel += 1
             else:
                 self.__channel = self.MIN_CHANNEL
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Method to decrease television channel
+        """
         if self.__status:
             if self.__channel > self.MIN_CHANNEL:
                 self.__channel -= 1
             else:
                 self.__channel = self.MAX_CHANNEL
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Method to decrease television volume
+        """
         if self.__status:
             self.__muted = False
             if self.__volume > self.MIN_VOLUME:
                 self.__volume -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Method to increase television volume
+        """
         if self.__status:
             self.__muted = False
             if self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Method to show television status
+        :return: tv status.
+        """
         if self.__muted:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.MIN_VOLUME}'
         else:
